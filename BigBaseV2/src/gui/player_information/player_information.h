11d3c530bb110af3f1a04f85011ebcb9a52d41b4
skript023 <fatur_muhammad@bk.ru>
2021-10-30 13:44:54 +0700

    Pembaruan Model

---
+++ BigBaseV2/src/gui/player_information/player_information.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "common.hpp"
+#include "natives.hpp"
+
+namespace big
+{
+	class player_information
+	{
+	public:
+		static void render_player_info();
+		static void render_player_event();
+	};
+}
